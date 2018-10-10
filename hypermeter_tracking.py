eb2b1cefd2ea13e139a28765324f3144912574c7
Jordan Smith <jblsmith@gmail.com>
2018-10-10 12:14:45 +0100

    utils.py: typo-fixes. Added ismir-presentation variation on presentation. hypermeter_tracking.py: deleted functions now in utils

--- hypermeter_tracking.py
+++ hypermeter_tracking.py
@@ -1,66 +1,42 @@
-import numpy as np
-import scipy as sp
-import madmom
-import librosa, librosa.display
-import mir_eval
-import pytube
-import matplotlib.pyplot as plt
-plt.ion()
-from matplotlib import gridspec
-import collections
-from scipy.spatial.distance import euclidean, cosine
 
-# Load audio
-def load_audio(path):
-	y, sr = librosa.core.load(path, sr=None, mono=False)
-	return y, sr
+# new trick! joblib version of the above, for quick computation and saving:
 
-# Load audio from YouTube!
-def load_audio_from_youtube(youtube_id):
-	# The YouTube ID is an 11-character string.
-	# Download file
-	destination_folder = "./"
-	print "Downloading from YouTube..."
-	destination_path = pytube.YouTube('http://youtube.com/watch?v=' + youtube_id).streams.first().download(destination_folder)
-	# TODO: refine above to avoid overwriting indiscriminately, and to get preferred format?
-	print "Saved to: " + destination_path
-	print "Loading audio..."
-	y, sr = load_audio(destination_path)
-	return y, sr, destination_path
+from joblib import Memory
+mem = Memory(cachedir='~/Documents/repositories/hypermeter/')
 
-def audio_to_madmom_ddf(y, savefile_stem=None):
-	if savefile_stem is not None:
-		if os.path.exists(savefile_stem + "_ddf.mat"):
-			ddf = sp.io.loadmat(savefile_stem + "_ddf")['ddf']
-			return ddf
+@mem.cache()
+def get_detection_function(y_mono):
+    return madmom.features.beats.RNNDownBeatProcessor()(y_mono)
+
+@mem.cache()
+def audiopath_to_ddf(filepath):
+	y, sr = load_audio(filepath)
 	y_mono = librosa.core.to_mono(y)
 	detection_function = madmom.features.beats.RNNDownBeatProcessor()(y_mono)
-	if savefile_stem is not None:
-		sp.io.savemat(savefile_stem, {'ddf':detection_function})
 	return detection_function
 
-def madmom_ddf_to_downbeats(detection_function, sr, bar_opts=[3,4]):
-	fps_ratio = sr * 1.0 / 44100
-	downbeat_estimates = madmom.features.beats.DBNDownBeatTrackingProcessor(beats_per_bar=bar_opts, fps=int(100*fps_ratio))(detection_function)
-	return downbeat_estimates
+audiopath_to_ddf("./10 The Stars.mp3")
+audiopath_to_ddf("/Users/jordan/Dropbox/Ircam/hackday/10 The Stars.mp3")
+neat_song_paths = ["/Users/jordan/Music/iTunes/iTunes Music/Rubblebucket/Omega La La/04 Came Out Of A Lady.m4a",
+	"/Users/jordan/Music/iTunes/iTunes Music/Final Fantasy/Spectrum, 14th Century/01 Oh Spectrum.mp3",
+	"/Users/jordan/Music/iTunes/iTunes Music/Sebastien Tellier/Politics/05 La ritournelle.mp3",
+	"/Users/jordan/Music/iTunes/iTunes Music/Paul Simon/Rhythm Of The Saints/03 The Coast.mp3",
+	"/Users/jordan/Music/iTunes/iTunes Music/Paul Simon/Rhythm Of The Saints/08 The Cool, Cool River.mp3",
+	"/Users/jordan/Music/iTunes/iTunes Music/Paul Simon/Rhythm Of The Saints/10 Rhythm Of The Saints.mp3"]
 
-def madmom_ddf_to_beats(detection_function, sr):
-	fps_ratio = sr * 1.0 / 44100
-	beat_estimates = madmom.features.beats.DBNBeatTrackingProcessor(fps=int(100*fps_ratio))(detection_function)
-	return beat_estimates
+for songpath in neat_song_paths:
+	audiopath_to_ddf(songpath)
 
-def get_ddf_feats_from_bar_onsets(ddf, bar_onsets):
-	feats_i = []
-	for t in range(len(bar_onsets_i)-1):
-		t1 = int(100*bar_onsets_i[t])
-		t2 = int(100*bar_onsets_i[t+1])
-		feature_seq_t = ddf[t1:t2,:]
-		feats_i += [feature_seq_t[:,1]]   # Take just the 2nd column, which is the downbeat detection function. 1st col has beat detection function.
-	return feats_i
+
+
+ddf = audiopath_to_ddf(neat_song_paths[5])
+savefile_stem = "the_cool_cool_river"
+# detection_function = get_detection_function(y_mono)```
 
 # Get audio from Youtube or local directory:
 you_make_me = "I98OWrboh-M"
-y, sr, filepath = load_audio_from_youtube(you_make_me)
+silence = "RZYpzXjdtwg"
+y, sr, filepath = load_audio_from_youtube(rapunzel)
 filepath = "./01 Hey Ya.mp3"
 filepath = "./10 The Stars.mp3"
 filepath = "./Weird Al Yankovic Even Worse - You Make Me.mp4"
@@ -70,15 +46,53 @@ savefile_stem = os.path.splitext(os.path.basename(filepath))[0]
 # Do beat tracking using madmom
 ddf = audio_to_madmom_ddf(y, savefile_stem)
 
+plt.figure(1),plt.clf()
+plt.plot(-ddf[:,0])
+plt.plot(ddf[:,1])
+plt.xlim([7860,9250])
+
 # The downbeat tracking of this madmom function assumes a constant bar length.
 # So if any bars are shortened or lengthened, or the time signature changes, the phase of the estimated downbeats will be thrown off.
 # Let's compare every pair of bars, considering every different initial offset in beats.
 # We will characterize each bar by its DDF.
-beat_est = madmom_ddf_to_downbeats(ddf, sr, bar_opts=[3,4])
+
+# Use downbeat tracking:
+beat_est = madmom_ddf_to_downbeats(ddf, sr, bar_opts=[3,4,5,6,7,8,9])
+bar_length = int(np.max(beat_est[:,1]))
+
+# Use beat tracking
 beat_est_only = madmom_ddf_to_beats(ddf[:,0], sr)
 beat_est = np.stack((beat_est_only, np.zeros_like(beat_est_only)), axis=1)
-bar_length = int(np.max(beat_est[:,1]))
-bar_length = 4
+
+dist_set = []
+for bar_length in range(2,9):
+	print bar_length
+	bar_onsets = [beat_est[range(i,beat_est.shape[0],bar_length),0] for i in range(bar_length)]
+	feats = [get_ddf_feats_from_bar_onsets(ddf, onsets) for onsets in bar_onsets]
+	dists = compute_dists_from_ddf_feats(feats)
+	dist_set += [dists]
+
+
+plt.figure(1)
+plt.clf()
+for i,dists in enumerate(dist_set):
+	plt.subplot(3,3,i+1)
+	plt.imshow(np.argmin(dists,axis=0))
+	plt.title(str(i+2)+"/4")
+	plt.colorbar()
+
+
+	plt.figure(3)
+	# make "phase-invariant" bar-similarity matrix
+	plt.imshow(np.argmin(dists,axis=0))
+	plt.title("Argmin across all Phase-0-Phase-X plots")
+	plt.colorbar()
+	plt.tight_layout()
+	plt.savefig(savefile_stem + "_argmin_phase_invariant.jpg")
+	
+	
+bar_length = 9
+
 # Generate different hypothesis downbeat streams
 bar_onsets = [beat_est[range(i,beat_est.shape[0],bar_length),0] for i in range(bar_length)]
 # For each, collect DDF excerpts as our features.
@@ -96,26 +110,13 @@ collections.Counter([len(x) for x in feats[0]])
 # To start, look at truncations:
 # Generate self-similarity matrix for all paris of beat-stream-feature_sets
 
-def compute_dists_from_ddf_feats(feats):
-	bar_length = len(feats)
-	n_bars = len(feats[0])
-	dists = np.zeros((bar_length, n_bars, n_bars))
-	for i in range(bar_length):
-		for k_x in range(n_bars-1):
-			for k_y in range(n_bars-1):
-				x = feats[0][k_x]
-				y = feats[i][k_y]
-				if len(x)>0 and len(y)>0:
-					min_len = np.min((len(x),len(y)))
-					dists[i,k_x,k_y] = cosine(x[:min_len], y[:min_len])
-	return dists
 
 
 dists = compute_dists_from_ddf_feats(feats)
-dists = dists[:,:80,:80]
+# dists = dists[:,:80,:80]
 plt.figure(1)
-for i in range(4):
-	plt.subplot(2,2,i+1)
+for i in range(9):
+	plt.subplot(3,3,i+1)
 	plt.imshow(dists[i,:,:])
 	plt.title("Phase-0 vs. Phase-"+str(i))
 
@@ -131,7 +132,8 @@ plt.savefig(savefile_stem + "_phase_invariant.jpg")
 plt.figure(3)
 # make "phase-invariant" bar-similarity matrix
 plt.imshow(np.argmin(dists,axis=0))
-plt.title("Min across all Phase-0-Phase-X plots")
+plt.title("Argmin across all Phase-0-Phase-X plots")
+plt.colorbar()
 plt.tight_layout()
 plt.savefig(savefile_stem + "_argmin_phase_invariant.jpg")
 
@@ -204,4 +206,3 @@ for beat_file in beat_files:
 
 for i in odd_songs.keys():
 	print odd_songs[i]
-
--- utils.py
+++ utils.py
@@ -9,6 +9,7 @@ plt.ion()
 from matplotlib import gridspec
 import collections
 from scipy.spatial.distance import euclidean, cosine
+import os.path
 
 # Load audio
 def load_audio(path):
@@ -36,7 +37,7 @@ def audio_to_madmom_ddf(y, savefile_stem=None):
 	y_mono = librosa.core.to_mono(y)
 	detection_function = madmom.features.beats.RNNDownBeatProcessor()(y_mono)
 	if savefile_stem is not None:
-		sp.io.savemat(savefile_stem, {'ddf':detection_function})
+		sp.io.savemat(savefile_stem + "_ddf", {'ddf':detection_function})
 	return detection_function
 
 def madmom_ddf_to_downbeats(detection_function, sr, bar_opts=[3,4]):
@@ -49,7 +50,7 @@ def madmom_ddf_to_beats(detection_function, sr):
 	beat_estimates = madmom.features.beats.DBNBeatTrackingProcessor(fps=int(100*fps_ratio))(detection_function)
 	return beat_estimates
 
-def get_ddf_feats_from_bar_onsets(ddf, bar_onsets):
+def get_ddf_feats_from_bar_onsets(ddf, bar_onsets_i):
 	feats_i = []
 	for t in range(len(bar_onsets_i)-1):
 		t1 = int(100*bar_onsets_i[t])
@@ -58,6 +59,7 @@ def get_ddf_feats_from_bar_onsets(ddf, bar_onsets):
 		feats_i += [feature_seq_t[:,1]]   # Take just the 2nd column, which is the downbeat detection function. 1st col has beat detection function.
 	return feats_i
 
+# FIXME: this function is inefficient! Candidate for streamlining.
 def compute_dists_from_ddf_feats(feats):
 	bar_length = len(feats)
 	n_bars = len(feats[0])
@@ -69,5 +71,8 @@ def compute_dists_from_ddf_feats(feats):
 				y = feats[i][k_y]
 				if len(x)>0 and len(y)>0:
 					min_len = np.min((len(x),len(y)))
+					# Sometimes the min_len is >0 but really small.
+					# Can we fix a sensible min_len for the entire piece, and then ignore a measure if it's too short?
+					# if min_len>80:
 					dists[i,k_x,k_y] = cosine(x[:min_len], y[:min_len])
 	return dists
